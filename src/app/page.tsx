e6ce55d18957b7b6286d89f44d30ef6817d0bbc3
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-06-21 14:46:07 +0000

    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { NavbarDemo } from "../components/ui/navbar-menu";
 import Image from "next/image";
 import { BackgroundCellAnimation } from "@/components/ui/BackgroundRippleEffect";
+import { StickyScrollRevealDemo } from "@/components/ui/sticky-scroll-reveal";
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
     <main >
       <NavbarDemo />
       <BackgroundCellAnimation />
+      <StickyScrollRevealDemo />
       
     </main>
   );
