af908617c239d991dab68a08a15656109e201ad7
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-06-21 12:19:15 +0000

    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import { NavbarDemo } from "../components/ui/navbar-menu";
 import Image from "next/image";
 import { BackgroundCellAnimation } from "@/components/ui/BackgroundRippleEffect";
+import { OrbitingCirclesDemo } from "@/components/magicui/orbiting-circles";
 
 export default function Home() {
   return (
     <main >
       <NavbarDemo />
       <BackgroundCellAnimation />
+      <OrbitingCirclesDemo/>
     </main>
   );
 }
