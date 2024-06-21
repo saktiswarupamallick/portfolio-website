40f7b7cb063a169babae4f964854054e11ee1eb6
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-06-21 18:26:25 +0000

    #

--- src/components/ui/sticky-scroll-reveal.tsx
+++ src/components/ui/sticky-scroll-reveal.tsx
@@ -6,14 +6,15 @@ import { cn } from "../../app/utils/cn";
 import Image from "next/image";
 
 const content = [
+ 
   {
-    title: "Real-time changes",
+    title: "Real time changes",
     description:
-      "See changes as they happen. With our platform, you can track every modification in real-time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+      "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
     content: (
-      <div className="h-full w-full flex items-center justify-center text-black">
+      <div className="h-full w-full  flex items-center justify-center text-white">
         <Image
-          src="/linear.webp"
+          src="/mypic.jpeg"
           width={300}
           height={300}
           className="h-full w-full object-cover"
@@ -23,13 +24,13 @@ const content = [
     ),
   },
   {
-    title: "Real-time changes",
+    title: "Real time changes",
     description:
-      "See changes as they happen. With our platform, you can track every modification in real-time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+      "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
     content: (
-      <div className="h-full w-full flex items-center justify-center text-black">
+      <div className="h-full w-full  flex items-center justify-center text-white">
         <Image
-          src="/linear.webp"
+          src="/certificate.jpg"
           width={300}
           height={300}
           className="h-full w-full object-cover"
@@ -38,15 +39,16 @@ const content = [
       </div>
     ),
   },
+ 
 ];
-
 export function StickyScrollRevealDemo() {
   return (
-    <div className="p-10 mt-[-150px]">
+    <div className="p-10 mt-[-160px]">
       <StickyScroll content={content} />
     </div>
   );
 }
+
 
 export const StickyScroll = ({
   content,
@@ -59,9 +61,11 @@ export const StickyScroll = ({
   }[];
   contentClassName?: string;
 }) => {
-  const [activeCard, setActiveCard] = useState(0);
+  const [activeCard, setActiveCard] = React.useState(0);
   const ref = useRef<any>(null);
   const { scrollYProgress } = useScroll({
+    // uncomment line 22 and comment line 23 if you DONT want the overflow container and want to have it change on the entire page scroll
+    // target: ref
     container: ref,
     offset: ["start start", "end start"],
   });
@@ -106,42 +110,44 @@ export const StickyScroll = ({
       animate={{
         backgroundColor: backgroundColors[activeCard % backgroundColors.length],
       }}
-      className="h-[30rem] w-full overflow-y-auto flex flex-col lg:flex-row justify-center relative space-y-10 lg:space-y-0 lg:space-x-10 rounded-md p-10"
+      className="h-[30rem] overflow-y-auto flex justify-center relative space-x-10 rounded-md p-10"
       ref={ref}
     >
-      <div className="relative flex flex-col items-start px-4 w-full max-w-4xl">
-        {content.map((item, index) => (
-          <div key={item.title + index} className="my-20">
-            <motion.h2
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: activeCard === index ? 1 : 0.3,
-              }}
-              className="text-3xl font-bold text-black"
-            >
-              {item.title}
-            </motion.h2>
-            <motion.p
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: activeCard === index ? 1 : 0.3,
-              }}
-              className="text-lg text-black max-w-sm mt-4"
-            >
-              {item.description}
-            </motion.p>
-          </div>
-        ))}
-        <div className="h-40" />
+      <div className="div relative flex items-start px-4">
+        <div className="max-w-2xl">
+          {content.map((item, index) => (
+            <div key={item.title + index} className="my-20">
+              <motion.h2
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: activeCard === index ? 1 : 0.3,
+                }}
+                className="text-3xl font-bold text-black"
+              >
+                {item.title}
+              </motion.h2>
+              <motion.p
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: activeCard === index ? 1 : 0.3,
+                }}
+                className="text-kg text-slate-900 max-w-sm mt-10"
+              >
+                {item.description}
+              </motion.p>
+            </div>
+          ))}
+          <div className="h-40" />
+        </div>
       </div>
       <div
         style={{ background: backgroundGradient }}
         className={cn(
-          "sticky top-10 h-96 w-full lg:w-80 rounded-md bg-white overflow-hidden",
+          "hidden lg:block h-60 w-80 rounded-md bg-white sticky top-10 overflow-hidden",
           contentClassName
         )}
       >
