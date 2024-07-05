53c354a1eb6fe03cafdbb0291cf7675c56e5964d
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-07-05 12:17:25 +0000

    #

---
+++ src/components/ui/following-pointer.tsx
@@ -0,0 +1,210 @@
+// Core component that receives mouse positions and renders pointer and content
+
+import React, { useEffect, useState } from "react";
+import { motion, AnimatePresence, useMotionValue } from "framer-motion";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+
+export function FollowingPointerDemo() {
+  return (
+    <div className="w-80 mx-auto">
+      <FollowerPointerCard
+        title={
+          <TitleComponent
+            title={blogContent.author}
+            avatar={blogContent.authorAvatar}
+          />
+        }
+      >
+        <div className="relative overflow-hidden h-full rounded-2xl transition duration-200 group bg-white hover:shadow-xl border border-zinc-100">
+          <div className="w-full aspect-w-16 aspect-h-10 bg-gray-100 rounded-tr-lg rounded-tl-lg overflow-hidden xl:aspect-w-16 xl:aspect-h-10 relative">
+            <Image
+              src="./certificate.jpg"
+              alt="thumbnail"
+              layout="fill"
+              objectFit="cover"
+              className={`group-hover:scale-95 group-hover:rounded-2xl transform object-cover transition duration-200 `}
+            />
+          </div>
+          <div className=" p-4">
+            <h2 className="font-bold my-4 text-lg text-zinc-700">
+              {blogContent.title}
+            </h2>
+            <h2 className="font-normal my-4 text-sm text-zinc-500">
+              {blogContent.description}
+            </h2>
+            <div className="flex flex-row justify-between items-center mt-10">
+              <span className="text-sm text-gray-500">{blogContent.date}</span>
+              <div className="relative z-10 px-6 py-2 bg-black text-white font-bold rounded-xl block text-xs">
+                Read More
+              </div>
+            </div>
+          </div>
+        </div>
+      </FollowerPointerCard>
+    </div>
+  );
+}
+
+const blogContent = {
+  slug: "amazing-tailwindcss-grid-layouts",
+  author: "Manu Arora",
+  date: "28th March, 2023",
+  title: "Amazing Tailwindcss Grid Layout Examples",
+  description:
+    "Grids are cool, but Tailwindcss grids are cooler. In this article, we will learn how to create amazing Grid layouts with Tailwindcs grid and React.",
+  image: "/demo/thumbnail.png",
+  authorAvatar: "/manu.png",
+};
+
+const TitleComponent = ({
+  title,
+  avatar,
+}: {
+  title: string;
+  avatar: string;
+}) => (
+  <div className="flex space-x-2 items-center">
+    <Image
+      src={avatar}
+      height="20"
+      width="20"
+      alt="thumbnail"
+      className="rounded-full border-2 border-white"
+    />
+    <p>{title}</p>
+  </div>
+);
+
+export const FollowerPointerCard = ({
+  children,
+  className,
+  title,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  title?: string | React.ReactNode;
+}) => {
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  const ref = React.useRef<HTMLDivElement>(null);
+  const [rect, setRect] = useState<DOMRect | null>(null);
+  const [isInside, setIsInside] = useState<boolean>(false); // Add this line
+
+  useEffect(() => {
+    if (ref.current) {
+      setRect(ref.current.getBoundingClientRect());
+    }
+  }, []);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (rect) {
+      const scrollX = window.scrollX;
+      const scrollY = window.scrollY;
+      x.set(e.clientX - rect.left + scrollX);
+      y.set(e.clientY - rect.top + scrollY);
+    }
+  };
+  const handleMouseLeave = () => {
+    setIsInside(false);
+  };
+
+  const handleMouseEnter = () => {
+    setIsInside(true);
+  };
+  return (
+    <div
+      onMouseLeave={handleMouseLeave}
+      onMouseEnter={handleMouseEnter}
+      onMouseMove={handleMouseMove}
+      style={{
+        cursor: "none",
+      }}
+      ref={ref}
+      className={cn("relative", className)}
+    >
+      <AnimatePresence>
+        {isInside && <FollowPointer x={x} y={y} title={title} />}
+      </AnimatePresence>
+      {children}
+    </div>
+  );
+};
+
+export const FollowPointer = ({
+  x,
+  y,
+  title,
+}: {
+  x: any;
+  y: any;
+  title?: string | React.ReactNode;
+}) => {
+  const colors = [
+    "var(--sky-500)",
+    "var(--neutral-500)",
+    "var(--teal-500)",
+    "var(--green-500)",
+    "var(--blue-500)",
+    "var(--red-500)",
+    "var(--yellow-500)",
+  ];
+  return (
+    <motion.div
+      className="h-4 w-4 rounded-full absolute z-50"
+      style={{
+        top: y,
+        left: x,
+        pointerEvents: "none",
+      }}
+      initial={{
+        scale: 1,
+        opacity: 1,
+      }}
+      animate={{
+        scale: 1,
+        opacity: 1,
+      }}
+      exit={{
+        scale: 0,
+        opacity: 0,
+      }}
+    >
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth="1"
+        viewBox="0 0 16 16"
+        className="h-6 w-6 text-sky-500 transform -rotate-[70deg] -translate-x-[12px] -translate-y-[10px] stroke-sky-600"
+        height="1em"
+        width="1em"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"></path>
+      </svg>
+      <motion.div
+        style={{
+          backgroundColor: colors[Math.floor(Math.random() * colors.length)],
+        }}
+        initial={{
+          scale: 0.5,
+          opacity: 0,
+        }}
+        animate={{
+          scale: 1,
+          opacity: 1,
+        }}
+        exit={{
+          scale: 0.5,
+          opacity: 0,
+        }}
+        className={
+          "px-2 py-2 bg-neutral-200 text-white whitespace-nowrap min-w-max text-xs rounded-full"
+        }
+      >
+        {title || `William Shakespeare`}
+      </motion.div>
+    </motion.div>
+  );
+};
